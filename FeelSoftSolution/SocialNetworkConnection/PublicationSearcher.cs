328a63b8523adbf6f40c88a21c9875e9d8ff9c47
Miguel Torres <miguelangeltorres657@hotmail.com>
2018-03-12 01:51:10 -0500

    Initial commit
    Load partial classes with their UML diagram.

---
+++ FeelSoftSolution/SocialNetworkConnection/PublicationSearcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SocialNetworkConnection
+{
+    public abstract class PublicationSearcher
+    {
+        private IEnumerable<IPublication> publications;
+
+        private Thread searchThread;
+
+        public SearchPublications SearchPublications
+        {
+            get => default(SearchPublications);
+            set
+            {
+            }
+        }
+
+        public IEnumerable<IPublication> IPublication
+        {
+            get => publications;
+            
+        }
+    }
+}
---
+++ FeelSoftSolution/SocialNetworkConnection/SocialNetwork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocialNetworkConnection
+{
+    public abstract class SocialNetwork : ISocialNetwork
+    {
+        private IQueryConfiguration[] queryConfigurations;
+        private string name;
+        private PublicationSearcher[] searchers;
+
+
+        public string Name
+        {
+            get => name;
+        }
+
+        public IQueryConfiguration[] QueryConfigurations
+        {
+            get => queryConfigurations;
+            set
+            {
+                queryConfigurations = value;
+            }
+        }
+
+        public PublicationSearcher[] PublicationSearcher
+        {
+            get => searchers;
+            set
+            {
+                searchers = value;
+            }
+        }
+    }
+}

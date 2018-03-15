e29898c36b7cec7e2f4be6885750c38f47c543cf
Miguel Torres <miguelangeltorres657@hotmail.com>
2018-03-15 02:26:26 -0500

    Added WebScrapperViewer

---
+++ FeelSoftSolution/View/WebScrapperViewer.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace View
+{
+    partial class WebScrapperViewer
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.publicationViewerControl1 = new View.PublicationViewerControl();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(23, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.publicationViewerControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(836, 394);
+            this.splitContainer1.SplitterDistance = 536;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // publicationViewerControl1
+            // 
+            this.publicationViewerControl1.Location = new System.Drawing.Point(44, 12);
+            this.publicationViewerControl1.Name = "publicationViewerControl1";
+            this.publicationViewerControl1.Size = new System.Drawing.Size(452, 365);
+            this.publicationViewerControl1.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 410);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private PublicationViewerControl publicationViewerControl1;
+    }
+}
+

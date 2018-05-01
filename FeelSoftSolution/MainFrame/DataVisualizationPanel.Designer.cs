d7f780c62a3b715dbcdcc178e5ec44d187d6eaac
BayronCampaz <35868120+BayronCampaz@users.noreply.github.com>
2018-05-01 17:07:53 -0500

    Add MainFrame project

---
+++ FeelSoftSolution/MainFrame/DataVisualizationPanel.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace MainFrame
+{
+    partial class DataVisualizationPanel
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartData
+            // 
+            this.chartData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.chartData.BackImageTransparentColor = System.Drawing.Color.Gainsboro;
+            this.chartData.BackSecondaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.chartData.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            chartArea1.Name = "ChartArea1";
+            this.chartData.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartData.Legends.Add(legend1);
+            this.chartData.Location = new System.Drawing.Point(29, 22);
+            this.chartData.Name = "chartData";
+            series1.BorderColor = System.Drawing.Color.Gray;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartData.Series.Add(series1);
+            this.chartData.Size = new System.Drawing.Size(687, 543);
+            this.chartData.TabIndex = 0;
+            this.chartData.Text = "chart1";
+            // 
+            // DataVisualizationPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.Controls.Add(this.chartData);
+            this.Name = "DataVisualizationPanel";
+            this.Size = new System.Drawing.Size(746, 606);
+            ((System.ComponentModel.ISupportInitialize)(this.chartData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartData;
+    }
+}
---
+++ FeelSoftSolution/MainFrame/MainFrame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Controller;
+
+
+namespace MainFrame
+{
+    public partial class MainFrame : Form
+    {
+        public MainFrame()
+        {
+            InitializeComponent();
+            InitializeControls();
+            controller = new Controller.Controller("..//..//..//Database//LemmatizedPublications//Petro");
+            ShowFormHome();
+
+        }
+
+        private void ShowFormHome()
+        {
+
+            containerPanel.Tag = home;
+            home.Show();
+        }
+
+        private void btnReports_Click(object sender, EventArgs e)
+        {
+            DateTime time = new DateTime(2018, 3, 10);
+            DateTime time2 = new DateTime(2018, 3, 28);
+            DateTime time3 = new DateTime(2018, 3, 27);
+
+            Dictionary<DateTime, double[]> dateAndCalification = controller.DailyAnalysis(time, time2);
+
+            MessageBox.Show(time3.ToShortDateString() + "\n" + dateAndCalification[time3][0] + "\n" + dateAndCalification[time3][1]);
+            
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            webScrapperViewer = new View.WebScrapperViewer();
+            webScrapperViewer.Show();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            ShowFormHome();
+        }
+
+
+        private void InitializeControls()
+        {
+            home = new HomePanel();
+            home.Dock = DockStyle.Fill;
+            home.TopLevel = false;
+            containerPanel.Controls.Add(home);
+        }
+    }
+}
